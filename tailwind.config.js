8969fe9f00257643de41c2f7308d59abad9ee928
afneybeyb <afneybeyb@gmail.com>
2023-01-07 23:52:42 +0100

    added new dark accent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "light": "#EFFBF7",
-        "dark": "#082118",
-        "accent": "#3CC8A0",
-        "accent-light": "#C0EEE0",
-      },
-    },
-    plugins: [],
-  }
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"light": "#EFFBF7",
+				"dark": "#12151C",
+				"accent": "#3CC8A0",
+				"accent-light": "#C0EEE0",
+				"accent-dark": "#0C3129",
+			},
+		},
+		plugins: [],
+	}
 }
