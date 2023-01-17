e84a33a66bac36f4474575cea3111f1d8d31ee42
afneybeyb <afneybeyb@gmail.com>
2023-01-17 14:41:12 +0100

    fixed mobile navigation so it is above everything when open

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -38,7 +38,7 @@ const Navigation = () => {
 
 	return (
 		/* navbar wrapper that centers navbar content */
-		<section className={`w-full h-full sm:h-auto fixed sm:static left-0 top-0 sm:flex sm:justify-center ${mobileNavOpen && "bg-accent-light/25 dark:bg-accent-dark/25 backdrop-blur"}`}>
+		<section className={`fixed sm:static left-0 top-0 sm:flex sm:justify-center ${mobileNavOpen ? "bg-accent-light/25 dark:bg-accent-dark/25 backdrop-blur z-20" : ""}`}>
 			{mobileNavOpen ? (
 				/* close navigation button for mobile */
 				<Button className="fixed right-0 top-0 p-6 text-accent hover:text-accent-dark dark:hover:text-accent-light hover:bg-transparent" onClick={() => setMobileNavOpen(false)}>
@@ -52,7 +52,7 @@ const Navigation = () => {
 			)}
 
 			{/* navbar */}
-			<nav className={`p-6 pb-4 h-full sm:h-auto ${mobileNavOpen ? "flex" : "hidden"} sm:flex flex-col sm:flex-row justify-center items-center gap-6 sm:gap-x-12 border-b-2 border-solid border-transparent sm:border-accent-light sm:dark:border-accent-dark`}>
+			<nav className={`p-6 pb-4 w-screen h-screen sm:h-auto ${mobileNavOpen ? "flex" : "hidden"} sm:flex flex-col sm:flex-row justify-center items-center gap-6 sm:gap-x-12 border-b-2 border-solid border-transparent sm:border-accent-light sm:dark:border-accent-dark`}>
 				{/* array map links */}
 				{links.map((link, i) => (
 					<Link className="dark:text-light hover:text-accent text-xl sm:text-base border-solid border-b-2 border-transparent hover:border-accent transition-colors duration-300" key={i} href={link.href} onClick={() => setMobileNavOpen(false)}>{link.label}</Link>
