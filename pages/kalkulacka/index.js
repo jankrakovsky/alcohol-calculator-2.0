e6c770a263e8ff008b240df04f20ea44d17f4478
afneybeyb <afneybeyb@gmail.com>
2023-02-18 17:04:41 +0100

    show drank drinks header only if there are some, code optimization

--- pages/kalkulacka/index.js
+++ pages/kalkulacka/index.js
@@ -1,4 +1,4 @@
-import {useMemo, useState} from "react";
+import {useCallback, useMemo, useState} from "react";
 import dayjs from "dayjs";
 import Head from 'next/head'
 import DrinkCard from "../../src/components/DrinkCard";
@@ -14,15 +14,15 @@ export default function Kalkulacka() {
         weight: "",
     });
 
-    const countTimeWhenSober = (totalAlcoholInBody, alcoholPerHour) => {
+    const countTimeWhenSober = useCallback((totalAlcoholInBody, alcoholPerHour) => {
         const endTime = dayjs(`1970-01-01T${userData.time}:00`);
         const howLongToSober = (totalAlcoholInBody / alcoholPerHour / userData.weight) * 60;
         const whenSober = endTime.add(howLongToSober, "minute");
 
         return whenSober;
-    }
+    }, [userData.time, userData.weight]);
 
-    const countTotalAlcoholInBody = () => {
+    const countTotalAlcoholInBody = useCallback(() => {
         const alcoholPerHour = userData.gender === "male" ? 0.1 : 0.085;
         const waterInBody = userData.gender === "male" ? 0.7 : 0.6;
 
@@ -38,10 +38,10 @@ export default function Kalkulacka() {
             soberTime,
             perMile
         }
-    }
+    }, [consumedDrinks, userData, countTimeWhenSober]);
 
     /* function that adds or edits selected drink in consumedDrinks array */
-    const setDrink = (selectedDrinkId, count = 1) => {
+    const setDrink = useCallback((selectedDrinkId, count = 1) => {
         /* Find the selected drink from all drinks */
         const selectedDrink = allDrinks.find(drink => drink.id === selectedDrinkId);
         /* if the selected drink does not exist, return */
@@ -62,10 +62,9 @@ export default function Kalkulacka() {
             /* if the selected drink does not exist in consumed drinks, add it with the given count */
             setConsumedDrinks([...consumedDrinks, {...selectedDrink, count}]);
         }
-    };
+    }, [consumedDrinks]);
 
-
-    const result = useMemo(() => countTotalAlcoholInBody(), [consumedDrinks, userData]);
+    const { soberTime, perMile } = useMemo(() => countTotalAlcoholInBody(), [countTotalAlcoholInBody]);
 
     return (
         <main className="h-full">
@@ -81,12 +80,12 @@ export default function Kalkulacka() {
                     <UserInfo userData={userData} setUserData={setUserData}/>
                 </article>
 
-                {result.perMile > 0 && (
+                {perMile > 0 && (
                     <article>
                         <h2 className="mb-4 text-2xl font-bold">Výsledek testu</h2>
 
-                        <p>Máte v krvi zhruba {result.perMile} promile alkoholu.</p>
-                        <p>Střízliví byste měli být v: {result.soberTime.format("HH:mm")}</p>
+                        <p>Máte v krvi zhruba {perMile} promile alkoholu.</p>
+                        <p>Střízliví byste měli být v: {soberTime.format("HH:mm")}</p>
                     </article>
                 )}
 
@@ -96,17 +95,19 @@ export default function Kalkulacka() {
                     <Search drinks={allDrinks} setDrink={setDrink}/>
                 </article>
 
-                {/* list of drinks you already drank */}
-                <article>
-                    <h2 className="mb-4 text-2xl font-bold">Vypité nápoje</h2>
+                {/* list of drinks you already drank (if any) */}
+                {consumedDrinks.length > 0 && (
+                    <article>
+                        <h2 className="mb-4 text-2xl font-bold">Vypité nápoje</h2>
 
-                    <span className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+                        <span className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
                         {/* show all consumed drinks in DrinkCard component */}
-                        {consumedDrinks.map((drink, index) => (
-                            <DrinkCard drink={drink} setDrink={setDrink} key={index}/>
-                        ))}
+                            {consumedDrinks.map((drink, index) => (
+                                <DrinkCard drink={drink} setDrink={setDrink} key={index}/>
+                            ))}
                     </span>
-                </article>
+                    </article>
+                )}
             </section>
         </main>
     )
