294da4fdc68316c4e13715f7c69ef5eb279bc79b
afneybeyb <afneybeyb@gmail.com>
2023-02-18 16:43:34 +0100

    turned off NextJS image optimization to work with Firebase Hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+/* FIXME: Find a way to use image optimization with Firebase Hosting */
 const nextConfig = {
 	reactStrictMode: true,
+	images: {
+		unoptimized: true,
+	},
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
