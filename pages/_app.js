73ceeed609fad8cb4b69df83ad7efa4f14e775fe
afneybeyb <afneybeyb@gmail.com>
2023-02-24 12:37:29 +0100

    set override of MUI styles with TailwindCSS styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,40 @@
 import Head from "next/head";
 import '../styles/globals.css';
 import Navigation from "../src/components/Navigation";
+import {StyledEngineProvider} from "@mui/material";
 
-export default function MyApp({ Component, pageProps }) {
-	/* current year to display in footer */
-	const currentYear = new Date().getFullYear();
+export default function MyApp({Component, pageProps}) {
+    /* current year to display in footer */
+    const currentYear = new Date().getFullYear();
 
-	return (
-		/* same for all pages */
-		<div className="h-full flex flex-col">
-			{/* header */}
-			<Head>
-				<title>Alkohol kalkulačka</title>
-				<meta name="description" content="Webová alkohol kalkulačka s chytrými funkcemi." />
+    return (
+        <StyledEngineProvider injectFirst>
+            {/* same for all pages */}
+            <div className="h-full flex flex-col">
+                {/* header */}
+                <Head>
+                    <title>Alkohol kalkulačka</title>
+                    <meta name="description" content="Webová alkohol kalkulačka s chytrými funkcemi."/>
 
-				<link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png" />
-				<link rel="manifest" href="/icon/site.webmanifest" />
-			</Head>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png"/>
+                    <link rel="manifest" href="/icon/site.webmanifest"/>
+                </Head>
 
-			<Navigation />
+                <Navigation/>
 
-			{/* main content */}
-			<div className="px-4 lg:px-12 xl:px-20 pt-20 pb-12 grow transition-all mx-auto max-w-screen-2xl">
-				<Component {...pageProps} />
-			</div>
+                {/* main content */}
+                <div className="px-4 lg:px-12 xl:px-20 pt-20 pb-12 grow transition-all mx-auto max-w-screen-2xl">
+                    <Component {...pageProps} />
+                </div>
 
-			{/* footer */}
-			<footer className="p-4 w-full border-t-2 border-solid border-accent-light dark:border-accent text-center">
-				<p>Copyright © {currentYear} | Alkohol kalkulačka - Všechna práva vyhrazena</p>
-			</footer>
-		</div >
-	);
+                {/* footer */}
+                <footer
+                    className="p-4 w-full border-t-2 border-solid border-accent-light dark:border-accent text-center">
+                    <p>Copyright © {currentYear} | Alkohol kalkulačka - Všechna práva vyhrazena</p>
+                </footer>
+            </div>
+        </StyledEngineProvider>
+    );
 }
