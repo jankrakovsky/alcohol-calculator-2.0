d33af754ab931519b3ef6d89c82fca8a27b40fe9
afneybeyb <afneybeyb@gmail.com>
2023-03-19 23:58:15 +0100

    extend HelperTooltip to use it to wrap ReactNode

--- src/components/HelperTooltip.js
+++ src/components/HelperTooltip.js
@@ -1,6 +1,7 @@
 import { Tooltip } from '@mui/material';
 
-const HelperTooltip = ({ title, text, placement = "bottom", ...props }) => (
+/* TODO: remove text - unify the component input */
+const HelperTooltip = ({ title, text, placement = "bottom", children, ...props }) => (
 	<Tooltip
 		className="cursor-help"
 		title={<span className="text-accent">
@@ -10,6 +11,7 @@ const HelperTooltip = ({ title, text, placement = "bottom", ...props }) => (
 		placement={placement}
 	>
 		<span className="border-b-2 border-dotted border-accent/75 hover:border-accent transition-colors" {...props}>
+			{children}
 			{text}
 		</span>
 	</Tooltip>
