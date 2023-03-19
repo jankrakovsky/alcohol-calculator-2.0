9bfbc68ac016961ca4ec5481f9ea84034db5e69b
afneybeyb <afneybeyb@gmail.com>
2023-03-19 23:57:37 +0100

    import Firestore to project

--- src/firebase/firebaseApp.ts
+++ src/firebase/firebaseApp.ts
@@ -1,5 +1,6 @@
 import {initializeApp} from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 
 /*  Firebase configuration */
 const firebaseConfig = {
@@ -14,7 +15,8 @@ const firebaseConfig = {
 /* Initialize Firebase */
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 
 
-export {auth, provider};
+export {auth, db, provider};
