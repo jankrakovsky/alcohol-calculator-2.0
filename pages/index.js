20f6295d3d41318dacadb064e63fc9d4c4bb0bec
afneybeyb <afneybeyb@gmail.com>
2023-03-19 23:59:52 +0100

    rewrite of site text

--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,34 @@ import Link from "next/link"
 import ImageParty from '../src/images/party.svg'
 
 export default function Home() {
-	return (
-		<main className="h-full flex flex-col justify-center items-center">
-			<section className="w-full grid gap-4 grid-cols-1 md:grid-cols-2 items-center">
-				<div className="flex flex-col gap-6 justify-center">
-					<h1 className="text-6xl text-accent dark:text-accent-light">Jsem Váš osobní rádce na alkohol!</h1>
-					<p className="text-2xl">
-						Potřebujete za volant či do práce?
-						Pro rychlé využití přejděte rovnou do kalkulačky, vyplňte informace a vše už uděláme za Vás.
-						Pro využítí všech našich funkcí se doporučujeme zaregistrovat.
-					</p>
-					<div className="flex gap-2">
-						<Link href="#">
-							<button className="p-4 font-semibold text-light dark:text-dark bg-accent border-accent border-2 border-solid rounded-lg">Přejít k registraci</button>
-						</Link>
+    return (
+        <main className="h-full flex flex-col justify-center items-center">
+            <section className="w-full grid gap-4 grid-cols-1 md:grid-cols-2 items-center">
+                <div className="flex flex-col gap-6 justify-center">
+                    <h1 className="text-6xl text-accent dark:text-accent-light">Jsem Váš osobní rádce na alkohol!</h1>
+                    <p className="text-2xl">
+                        Potřebujete za volant či do práce? Pro rychlé využití přejděte rovnou do kalkulačky, vyplňte
+                        informace a vše se vypočítá za Vás. Pro využítí všech funkcí se můžete přihlásit přes Google
+                        účet.
+                    </p>
+                    <div className="flex gap-2">
+                        <Link href="#">
+                            <button
+                                className="p-4 font-semibold text-light dark:text-dark bg-accent border-accent border-2 border-solid rounded-lg">Přihlásit
+                                se přes Google
+                            </button>
+                        </Link>
 
-						<Link href="/kalkulacka">
-							<button className="p-4 font-semibold border-accent border-2 border-solid rounded-lg">Přejít do kalkulačky</button>
-						</Link>
+                        <Link href="/kalkulacka">
+                            <button className="p-4 font-semibold border-accent border-2 border-solid rounded-lg">Přejít
+                                do kalkulačky
+                            </button>
+                        </Link>
 
-					</div>
-				</div>
-				<Image src={ImageParty} alt="People enjoying party" />
-			</section>
-		</main>
-	)
+                    </div>
+                </div>
+                <Image src={ImageParty} alt="People enjoying party"/>
+            </section>
+        </main>
+    )
 }
--- pages/o-projektu/index.js
+++ pages/o-projektu/index.js
@@ -10,7 +10,7 @@ export default function OProjektu() {
 
 			<section className="flex flex-col gap-6">
 				<article>
-					<h2 className="mb-4 text-2xl font-bold">Jak počítáme?</h2>
+					<h2 className="mb-4 text-2xl font-bold">Výpočet</h2>
 
 					<p>
 						<HelperTooltip title="Počet promile alkoholu v krvi [‰]" text="Alkohol v krvi" placement="top" />
@@ -50,22 +50,19 @@ export default function OProjektu() {
 				<article>
 					<h2 className="text-2xl font-bold">Proč vznikl tento projekt?</h2>
 					<p>
-						Tento projekt vznikl nejen jako studentský projekt, ale i jako nástroj pro analýzu vztahu člověka k alkoholu.
-						Alkohol je populární droga, je však potřeba jí mít pod kontrolou.
-						Naši službou pomáháme uživateli s informovaností ohledně trvání důsledků konzumace alkoholu.
-						Dále se snažíme uživateli dát prezentovat relevantní data k lepšímu uvědomění jeho/jejího vztahu k této návykové látce.
+						Tento projekt vznikl nejen jako studentský projekt ale i nástroj, který může lidem pomoci při rozhodování zda jsou schopni například dorazit do práce.
+						Alkohol je v České republice velice populární forma zábavy, je ale důležité přistupovat k alkoholu s respektem.
 					</p>
 				</article>
 
 				<article>
 					<h2 className="text-2xl font-bold">Jaká je přesnost dat?</h2>
 					<p>
-						Snažíme se poskytovat co nejpřsnější data z Vámi poskytnutých informací. Je důležité vyplnit požadavaná data co nejpřesněji, jelikož každý faktor ovlivňuje výsledek.
-						Pro největší přesnost je však nejlepší zakoupit alkoholmetr, který je nejpřesnějším řešení. Veškeré odpovědnosti za zkreslené výsledky se zříkáme.
+						Aplikace se snaží poskytovat co nejpřesnější data z Vámi poskytnutých informací. Je důležité vyplnit požadované informace co nejpřesněji, jelikož každý faktor ovlivňuje výsledek. Pro nejvyšší přesnost je však nutné zakoupit alkohol metr. Výsledky jsou pouze orientační. Veškeré odpovědnosti za zkreslené/nepřesné výsledky se zříkávám.
 					</p>
 				</article>
 
-				<h2 className="mt-8 text-2xl font-bold">Děkujeme Vám za využívání této webové aplikace!</h2>
+				<h2 className="mt-8 text-2xl font-bold">Děkuji Vám za využívání této webové aplikace!</h2>
 			</section>
 		</main >
 	)
